2e460f40d346068ba082eb04708ff08323a109ed
Patrik Galina <patrik.galina@king-ict.hr>
2022-06-16 14:39:15 +0200

    chore: Initialize project 🚀

---
+++ src/main/java/hr/pgalina/chain-reaction/ChainReactionApplication.java
@@ -0,0 +1,13 @@
+package hr.pgalina.chainreaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainReactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainReactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/pgalina/chainreaction/ChainReactionApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.pgalina.chainreaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainReactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
