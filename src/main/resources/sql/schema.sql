df7a0be56bce92408368d062793d00f98c2203e7
Patrik Galina <patrik.galina@king-ict.hr>
2022-07-19 19:25:18 +0200

    chore: Create product table

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS public.user;
-
 CREATE TABLE public.user
 (
     id_user BIGSERIAL PRIMARY KEY,
@@ -12,4 +11,25 @@ CREATE TABLE public.user
     modified_by VARCHAR(100) DEFAULT CURRENT_USER NOT NULL,
     modified_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE
+);
+
+DROP TABLE IF EXISTS public.product;
+CREATE TABLE public.product
+(
+    id_product BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    model VARCHAR(255),
+    assist_speed INTEGER,
+    battery_range INTEGER,
+    charging_time FLOAT,
+    weight FLOAT,
+    price DECIMAL(12,2) NOT NULL,
+    color SMALLINT,
+    available_quantity BIGINT NOT NULL,
+    type SMALLINT NOT NULL,
+    created_by VARCHAR(100) DEFAULT CURRENT_USER NOT NULL,
+    created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    modified_by VARCHAR(100) DEFAULT CURRENT_USER NOT NULL,
+    modified_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 );
