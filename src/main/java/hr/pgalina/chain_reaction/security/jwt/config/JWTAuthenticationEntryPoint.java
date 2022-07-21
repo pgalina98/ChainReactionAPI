81e0c650547d7026c7759eaeaf379cc986e58b67
Patrik Galina <patrik.galina@king-ict.hr>
2022-07-21 20:39:07 +0200

    chore: Change logic in commence method

--- src/main/java/hr/pgalina/chain_reaction/security/jwt/config/JWTAuthenticationEntryPoint.java
+++ src/main/java/hr/pgalina/chain_reaction/security/jwt/config/JWTAuthenticationEntryPoint.java
@@ -4,18 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import hr.pgalina.chain_reaction.domain.exception.BadRequestException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 import static hr.pgalina.chain_reaction.domain.exception.contants.ErrorTypeConstants.ERROR;
 import static hr.pgalina.chain_reaction.domain.exception.contants.ExceptionMessages.UNAUTHORIZED_REQUEST;
@@ -34,12 +30,7 @@ public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
     }
 
     @Override
-    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException ex) throws IOException {
-        ServerHttpResponse outputMessage = new ServletServerHttpResponse(httpServletResponse);
-        outputMessage.setStatusCode(HttpStatus.UNAUTHORIZED);
-
-        BadRequestException badRequestException = new BadRequestException(ERROR, HttpStatus.UNAUTHORIZED, UNAUTHORIZED_REQUEST);
-
-        messageConverter.write(mapper.writeValueAsString(badRequestException), MediaType.APPLICATION_JSON, outputMessage);
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException ex) throws BadRequestException {
+        throw new BadRequestException(ERROR, HttpStatus.UNAUTHORIZED, UNAUTHORIZED_REQUEST);
     }
 }
